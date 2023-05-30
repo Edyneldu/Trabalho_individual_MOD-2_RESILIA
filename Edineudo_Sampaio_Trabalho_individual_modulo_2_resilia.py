04a016912ae7c4dacd86529a551c5afc42065ca2
Edyneldu <132008446+Edyneldu@users.noreply.github.com>
2023-05-30 11:46:13 -0300

    Update Edineudo_Sampaio_Trabalho_individual_modulo_2_resilia.py
    up10

--- Edineudo_Sampaio_Trabalho_individual_modulo_2_resilia.py
+++ Edineudo_Sampaio_Trabalho_individual_modulo_2_resilia.py
@@ -2,7 +2,6 @@
 def titulo(msg):
     print('\n')
     print(msg)
-
 # Função para exibir a lista de candidatos
 def listaCandidato(candidato):
     titulo('Lista de Candidatos:')
@@ -13,18 +12,16 @@ def listaCandidato(candidato):
 def aprovados(criterios, candidato):
     # Lista para armazenar os candidatos aprovados
     listaAprovados = [f'{j+1}° Candidato e_{c[0]}_t{c[1]}_p{c[2]}_s{c[3]}' for j, c in enumerate(candidato) if all(0 < criterios[i] <= c[i] for i in range(4))]
-
     titulo('Aprovados:')
     # Verifica se não há candidatos aprovados
     if len(listaAprovados) == 0:
         print('\n_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX_\n')
-        print('Nenhum aprovado!')
+        print('_______________________________________________________________Nenhum Aprovado! _______________________________________________________')
         print('\n_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX_\n')
     else:
         # Exibe os candidatos aprovados
         for ap in listaAprovados:
             print(f'{ap[:13]:.<18}{ap[13:]}')
-
 # Função para obter uma nota digitada pelo usuário
 def obterNota(rotulo):
     while True:
@@ -35,8 +32,7 @@ def obterNota(rotulo):
         except ValueError:
             print('\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Atenção !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
             print('Por favor, digite um número válido!!!')
-            print('\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Warming !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
-
+            print('\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Warming !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
 # Função para obter a resposta do usuário (s/n)
 def obterResposta():
     while True:
@@ -47,43 +43,31 @@ def obterResposta():
         else:
             print('\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Atenção !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
             print('Por favor, digite uma resposta válida ("s" para "sim" ou "n" para "não")!!!')
-            print('\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WarminG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
-
+            print('\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WarminG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
 # Lista para armazenar os candidatos
 candidato = []
 pos = 1
-
 # Início do programa
-print('\n________________________________________________ Início _______________________________________________________________________________\n')
+print('\n_________________________________________________________________ Início ______________________________________________________________\n')
 # Exibe o título para inserção das notas
 titulo('Insira as notas do Candidato abaixo:')
-
 while True:
-    print(f'{pos}° Candidato:')
-    
+    print(f'{pos}° Candidato:') 
     # Obtém as notas do candidato
     notas = [obterNota(rotulo) for rotulo in ['Entrevista', 'Teste Teórico', 'Teste Prático', 'Soft Skill']]
     candidato.append(notas)
-
     # Obtém a resposta do usuário para adicionar mais candidatos
     resposta = obterResposta()
     if resposta == 'n':
         break
     pos += 1
-
 # Exibe o título para inserção dos critérios
 titulo('Quais são os Critérios:')
-
 # Obtém os critérios de avaliação
 criterios = [obterNota(rotulo) for rotulo in ['e_ Entrevista', 't_ Teste Teórico', 'p_ Teste Prático', 's_ Soft Skill']]
-
 # Exibe a lista de candidatos
 listaCandidato(candidato)
-
 # Verifica e exibe os candidatos aprovados
-print('\n________________________________________________________________________________________________________________________________________\n')
 aprovados(criterios, candidato)
-
-print('\n_____________________________________________________ FIM ______________________________________________________________________________\n')
-
+print('\n___________________________________________________________________ FIM ________________________________________________________________\n')
 #°2023 - Edineudo Sampaio - Resília/iFood
